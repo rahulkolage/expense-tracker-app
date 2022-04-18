151a0bbb32c2a28faec78febda472918a73817cb
Rahul Kolage <Rahul@Apples-MacBook-Pro.local>
2022-04-18 13:28:44 +0530

    code formatting

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import Expenses from "./components/Expenses/Expenses";
 
 const INITIAL_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e3",
+    title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e4",
+    title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
@@ -43,8 +43,8 @@ const App = () => {
     <div>
       {/* onAddExpense is pointer to output function addExpenseHandler */}
       {/* addExpenseHandler will get called inside NewExpense, accessed via Props in  NewExpense.js */}
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 
@@ -55,6 +55,6 @@ const App = () => {
   //   React.createElement('h2',{},'Let\'s get started!'),
   //   React.createElement(expenses,{items: expenses})
   // );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
-
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 // Stateless or presentational component
 // One rule, u must have on root element per return statement or per js code snippet
@@ -18,15 +17,15 @@ const ExpenseItem = (props) => {
     //   </div>
     // </div>
     <li>
-      <Card className='expense-item'>
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item__description'>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
           <h2>{props.title}</h2>
-          <div className='expense-item__price'>${props.amount}</div>
-        </div>    
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
       </Card>
     </li>
   );
-}
+};
 
 export default ExpenseItem;
